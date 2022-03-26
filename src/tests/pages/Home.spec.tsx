a9f386d039c45a909cc26080ecdc21037a5be553
Cosmo Rodrigues <cosmo.rsilva@gmail.com>
2022-03-26 14:12:40 -0300

    Starting tests

---
+++ src/tests/pages/Home.spec.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+
+const server = setupServer(
+  rest.get("/greeting", (req, res, ctx) => {
+    return res(ctx.json({ greeting: "hello there" }));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+describe("Home page", () => {
+  it.todo("Home");
+});
